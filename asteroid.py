04503eb5abf6104151a8a3ac20c79daf57202044
Nico Meier <nicomeier3@live.de>
2024-08-24 15:12:45 +0200

    Added base functionality of original game

--- asteroid.py
+++ asteroid.py
@@ -1,10 +1,11 @@
 import pygame
+import random
 
 from circleshape import CircleShape
+from constants import ASTEROID_MIN_RADIUS
 
 
-class Asteroid(CircleShape):
-    
+class Asteroid(CircleShape):  
     def __init__(self, x, y, radius):
         super().__init__(x, y, radius)
         
@@ -13,3 +14,18 @@ class Asteroid(CircleShape):
     
     def update(self, dt):
         self.position += self.velocity * dt
+        
+    def split(self):
+        super().kill()
+        if self.radius <= ASTEROID_MIN_RADIUS:
+            return
+        spawn_angle = random.uniform(20, 50)
+        angle1 = pygame.Vector2(self.velocity).rotate(spawn_angle)
+        angle2 = pygame.Vector2(self.velocity).rotate(-(spawn_angle))
+        new_radius = self.radius - ASTEROID_MIN_RADIUS
+        
+        asteroid_1 = Asteroid(self.position.x, self.position.y, new_radius)
+        asteroid_2 = Asteroid(self.position.x, self.position.y, new_radius)
+        
+        asteroid_1.velocity = angle1 * 2
+        asteroid_2.velocity = angle2 * 2
--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from constants import *
 from player import Player
 from asteroid import Asteroid
 from asteroidfield import AsteroidField
+from shot import Shot
 
 def main():
     print("Starting asteroids!")
@@ -18,10 +19,12 @@ def main():
     updateable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
     asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
     
     Player.containers = (updateable, drawable)
     Asteroid.containers = (updateable, drawable, asteroids)
     AsteroidField.containers = (updateable)
+    Shot.containers = (shots, updateable, drawable)
     
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     asteroid_field = AsteroidField()
@@ -36,12 +39,17 @@ def main():
         for u in updateable:
             u.update(dt)
             
+        # Check for asteroid collisons
         for a in asteroids:
             if player.has_collided(a):
                 print("Game over!")
                 sys.exit()
             
-        for d in drawable:
+            for s in shots:
+                if s.has_collided(a):
+                    a.split()
+            
+        for d in drawable: 
             d.draw(screen)      
         
         pygame.display.flip()
